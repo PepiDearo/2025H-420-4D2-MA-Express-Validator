633f4db567e95a8268d93491d6ddb2680c2cb1cd
PepiDearo <alexthach2004@gmail.com>
2025-04-03 10:38:56 -0400

    more

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body, validationResult } = require('express-validator');
+const { body,query, validationResult } = require('express-validator');
 const app = express();
 
 app.use(express.json());
@@ -11,10 +11,27 @@ app.get('/hello', (req, res) => {
 
 
 
-
+//body
 // Middleware de validation unique
+// const validationUtilisateur = [
+//   body('email')
+//     .isEmail().withMessage('L\'adresse email n\'est pas valide')
+//     .normalizeEmail(),
+//   body('password')
+//     .isLength({ min: 8 }).withMessage('Le mot de passe doit contenir au moins 8 caractères')
+//     .matches(/\d/).withMessage('Le mot de passe doit contenir au moins un chiffre')
+//     .matches(/[a-zA-Z]/).withMessage('Le mot de passe doit contenir au moins une lettre'),
+//   body('age')
+//     .isInt({ gt: 0 }).withMessage('L\'âge doit être un entier positif'),
+//   body('username')
+//     .trim()
+//     .isLength({ min: 3, max: 20 }).withMessage('Le nom d\'utilisateur doit contenir entre 3 et 20 caractères')
+// ];
+
+
+//query
 const validationUtilisateur = [
-  body('email')
+  query('email')
     .isEmail().withMessage('L\'adresse email n\'est pas valide')
     .normalizeEmail(),
   body('password')
@@ -28,8 +45,49 @@ const validationUtilisateur = [
     .isLength({ min: 3, max: 20 }).withMessage('Le nom d\'utilisateur doit contenir entre 3 et 20 caractères')
 ];
 
+
+
+
+
+
+
+
+
+
+
 //
 app.post('/signup', validationUtilisateur, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  res.send('Utilisateur valide');
+});
+
+
+
+
+
+
+// Middleware de validation unique avec query parameters
+const validateUserQuery = [
+  query('email')
+    .isEmail().withMessage('L\'adresse email n\'est pas valide')
+    .normalizeEmail(),
+  query('password')
+    .isLength({ min: 8 }).withMessage('Le mot de passe doit contenir au moins 8 caractères')
+    .matches(/\d/).withMessage('Le mot de passe doit contenir au moins un chiffre')
+    .matches(/[a-zA-Z]/).withMessage('Le mot de passe doit contenir au moins une lettre'),
+  query('age')
+    .isInt({ gt: 0 }).withMessage('L\'âge doit être un entier positif')
+    .toInt(),
+  query('username')
+    .trim()
+    .isLength({ min: 3, max: 20 }).withMessage('Le nom d\'utilisateur doit contenir entre 3 et 20 caractères')
+];
+
+// Route unique pour la validation avec query parameters
+app.get('/get_user', validateUserQuery, (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
@@ -40,5 +98,4 @@ app.post('/signup', validationUtilisateur, (req, res) => {
 app.listen(3000, () => {
   console.log('Serveur démarré sur le port 3000');
 });
-
 
